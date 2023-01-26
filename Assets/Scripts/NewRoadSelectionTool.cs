833c5afd0bd80a4439fca6cc253be4a792217d8f
bartekchrup <bartekchrup@gmail.com>
2023-01-26 16:08:57 +0000

    Add circle first selection

--- Assets/Scripts/NewRoadSelectionTool.cs
+++ Assets/Scripts/NewRoadSelectionTool.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class NewRoadSelectionTool : MonoBehaviour
 {
     [SerializeField] private NewRoadSelectionLine linePrefab;
+    [SerializeField] private GameObject selectionCircle;
     private Camera cam;
 
     private bool drawingLine;
     private NewRoadSelectionLine instantiantedLine;
+    // The user makes 2 selections to create a road segment
+    private GameObject firstSelectionCircle;
+    private GameObject secondSelectionCircle;
     private Vector2[] roadSegmentPoints;
 
     // Start is called before the first frame update
@@ -24,7 +28,6 @@ public class NewRoadSelectionTool : MonoBehaviour
         Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
         // Starting new selection
         if (Input.GetMouseButtonDown(0) && (! drawingLine)) {
-            Debug.Log("new line");
             instantiantedLine = Instantiate(linePrefab);
             instantiantedLine.SetStartingPoint(mousePosition);
             drawingLine = true;
@@ -33,12 +36,17 @@ public class NewRoadSelectionTool : MonoBehaviour
             roadSegmentPoints = instantiantedLine.GetLinePoints();
             // Once the selection has been made the line is no longer needed
             Destroy(instantiantedLine.gameObject);
-            Debug.Log("line deleted");
             drawingLine = false;
         }
         // Update the position of the 2nd point of the line when the mouse moves
         if (drawingLine) {
             instantiantedLine.SetSecondPoint(mousePosition);
+        } else {
+            if (firstSelectionCircle) {
+                firstSelectionCircle.transform.position = mousePosition;
+            } else  {
+                firstSelectionCircle = Instantiate(selectionCircle, mousePosition, Quaternion.identity);
+            }
         }
 
     }
